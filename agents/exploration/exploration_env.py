da4442cade74cee5789de41df69170ae65ad1ebc
Oleg Mürk <oleg.myrk@gmail.com>
2018-05-20 13:15:32 +0000

    support for turning on/off backtracking

--- agents/exploration/exploration_env.py
+++ agents/exploration/exploration_env.py
@@ -12,8 +12,9 @@ import cloudpickle
 import gzip
 
 class ExplorationEnv(gym.Wrapper):
-   def __init__(self, env, max_exploration_steps=None, state_encoder=None):
+   def __init__(self, env, allow_backtracking=True, max_exploration_steps=None, state_encoder=None):
      super(ExplorationEnv, self).__init__(env)
+     self.allow_backtracking = True
      self.max_exploration_steps = max_exploration_steps
      self.state_encoder = state_encoder
      self.episode=None
@@ -102,6 +103,11 @@ class ExplorationEnv(gym.Wrapper):
 
      self.total_extra_reward += extra_reward_scale * extra_reward
 
+     if self.allow_backtracking:
+       final_reward = adjusted_reward + extra_reward
+     else:
+       final_reward = reward + extra_reward
+
      timestamp = datetime.datetime.now()
      print("EXPLORE: timestamp=%s movie_id=%s total_steps=%s episode=%s episode_step=%s local_visited=%s global_visited=%s exploration_reward_weight=%s extra_reward_scale=%s exploration_local_reward=%s exploration_global_reward=%s extra_rings_reward=%s relative_x=%s relative_y=%s cell=%s embedding=%s" % (timestamp, self.get_movie_id(), self.total_steps, self.episode, self.episode_step, self.local_visited[cell_key], self.global_visited[cell_key], exploration_reward_weight, extra_reward_scale, exploration_local_reward, exploration_global_reward, extra_rings_reward, relative_x, relative_y, cell_key, state_embedding.tolist()))
      print("STEP: timestamp=%s movie_id=%s total_steps=%s episode=%s episode_step=%s action=%s reward=%s adjusted_reward=%s extra_reward=%s current_reward=%s current_adjusted_reward=%s current_extra_reward=%s info=%s" % (timestamp, self.get_movie_id(), self.total_steps, self.episode, self.episode_step, action, reward, adjusted_reward, extra_reward, self.total_reward, self.total_adjusted_reward, self.total_extra_reward, info))
@@ -137,5 +143,5 @@ class ExplorationEnv(gym.Wrapper):
 
      self.last_obs = obs
      self.last_info = info
-     return obs, adjusted_reward + extra_reward, done, info
+     return obs, final_reward, done, info
 
