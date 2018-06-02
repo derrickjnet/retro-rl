6f3d15c1876c18618e84adbbc5dde036951faf61
Oleg Mürk <oleg.myrk@gmail.com>
2018-06-02 21:58:45 +0000

    self-supervised prediciton improvements

--- agents/exploration/state_encoder.py
+++ agents/exploration/state_encoder.py
@@ -1,12 +1,15 @@
 import os
 import datetime
 import numpy as np
+import random
 import tensorflow as tf
 from exploration.autoencoder import Autoencoder 
 
 class StateEncoder:
-  def __init__(self, sess, num_actions, embedder_dir=os.environ.get('RETRO_STATE_EMBEDDER_DIR'), embedder_model_scope=os.environ.get('RETRO_STATE_EMBEDDER_MODEL_SCOPE', 'state_embedder'), embedder_nfilters=int(os.environ.get('RETRO_STATE_EMBEDDER_NFILTERS',32)), embedder_embedding_size=int(os.environ.get('RETRO_STATE_EMBEDDER_EMBEDDING_SIZE',256)), predictor_dir=os.environ.get('RETRO_STATE_PREDICTOR_DIR'), predictor_model_scope=os.environ.get('RETRO_STATE_PREDICTOR_MODEL_SCOPE', 'state_predictor'), predictor_nfilters=int(os.environ.get('RETRO_STATE_PREDICTOR_NFILTES', 32)), predictor_embedding_size=int(os.environ.get('RETRO_STATE_PREDICTOR_EMBEDDING_SIZE', 256))):
+  def __init__(self, sess, num_actions, num_images, embedder_dir=os.environ.get('RETRO_STATE_EMBEDDER_DIR'), embedder_model_scope=os.environ.get('RETRO_STATE_EMBEDDER_MODEL_SCOPE', 'state_embedder'), embedder_nfilters=int(os.environ.get('RETRO_STATE_EMBEDDER_NFILTERS',32)), embedder_embedding_size=int(os.environ.get('RETRO_STATE_EMBEDDER_EMBEDDING_SIZE',256)), predictor_dir=os.environ.get('RETRO_STATE_PREDICTOR_DIR'), predictor_model_scope=os.environ.get('RETRO_STATE_PREDICTOR_MODEL_SCOPE', 'state_predictor'), predictor_nfilters=int(os.environ.get('RETRO_STATE_PREDICTOR_NFILTES', 32)), predictor_embedding_size=int(os.environ.get('RETRO_STATE_PREDICTOR_EMBEDDING_SIZE', 256)), predictor_replay_size=int(os.environ.get("RETRO_STATE_PREDICTOR_REPLAY_SIZE", 20000)), predictor_batch_size=int(os.environ.get("RETRO_STATE_PREDICTOR_BATCH_SIZE", 1))):
     self.sess = sess
+    self.num_actions = num_actions
+    self.num_images = num_images
     self.embedder_dir = embedder_dir
     self.predictor_dir = predictor_dir
     if self.embedder_dir:
@@ -18,19 +21,79 @@ class StateEncoder:
     if self.predictor_dir:
       self.predictor = Autoencoder(nfilters=predictor_nfilters, embedding_size=predictor_embedding_size)
       self.predictor_model_scope = predictor_model_scope
-      with tf.variable_scope(predictor_model_scope, reuse=tf.AUTO_REUSE):
-        self.predictor_actions = tf.placeholder(tf.int32, [None])
-        self.predictor_model_obses1, _, self.predictor_model_embeddings1 = self.predictor.embed(embedding_activation=tf.nn.sigmoid, reuse=tf.AUTO_REUSE) 
-        self.predictor_model_obses2, _, self.predictor_model_embeddings2 = self.predictor.embed(embedding_activation=tf.nn.sigmoid, reuse=tf.AUTO_REUSE)
-      with tf.variable_scope(predictor_model_scope + "_internal"):
-        predictor_net = tf.layers.dense(tf.concat([self.predictor_model_embeddings1, self.predictor_model_embeddings2], -1), 256, activation=tf.nn.relu, name="fc1")
-        self.predictor_logits = tf.layers.dense(predictor_net, num_actions, name="fc2")
-        self.predictor_probs = tf.nn.softmax(self.predictor_logits,-1)
-        self.predictor_inverse_errors = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=self.predictor_logits, labels=self.predictor_actions)
-        self.predictor_train_loss = tf.reduce_mean(self.predictor_inverse_errors)
-        self.predictor_global_step = tf.train.create_global_step()
-        self.predictor_train_step = tf.train.AdamOptimizer().minimize(self.predictor_train_loss, global_step = self.predictor_global_step)
+      
+      predictor_model_builder = lambda predictor_rescaled_obses, predictor_actions: self.build_inverse_forward_predictor_model(predictor_rescaled_obses, predictor_actions)
+    
+      self.eval_predictor_model_obses, self.eval_predictor_actions, self.eval_predictor_model_rescaled_obses = self.build_predictor_placeholders()
+      self.eval_predictor_extras, self.eval_predictor_rewards, _ = predictor_model_builder(self.eval_predictor_model_rescaled_obses, self.eval_predictor_actions)
+    
+      self.train_predictor_model_obses, self.train_predictor_actions, self.train_predictor_model_rescaled_obses = self.build_predictor_placeholders()
+      self.train_predictor_extras, self.train_predictor_rewards, self.predictor_train_loss = predictor_model_builder(self.train_predictor_model_rescaled_obses, self.train_predictor_actions)
 
+      with tf.variable_scope(predictor_model_scope + "_train"):
+        self.predictor_global_step = tf.train.create_global_step()
+        self.predictor_train_step = tf.train.AdamOptimizer(learning_rate=0.0001).minimize(self.predictor_train_loss, global_step = self.predictor_global_step)
+        self.predictor_replay_buffer = []
+        self.predictor_replay_size = predictor_replay_size
+        self.predictor_batch_size = predictor_batch_size 
+
+  def build_predictor_placeholders(self):
+    with tf.variable_scope(self.predictor_model_scope, reuse=tf.AUTO_REUSE):
+      predictor_actions = tf.placeholder(tf.int32, [None])
+      with tf.variable_scope("observations"):
+        predictor_model_obses = tf.placeholder(tf.uint8, [None, 84, 84, self.num_images])
+      predictor_model_rescaled_obses = self.predictor.observations_rescaled(predictor_model_obses)
+    return predictor_model_obses, predictor_actions, predictor_model_rescaled_obses
+
+  def build_autoencoder_predictor_model(self, predictor_model_rescaled_obses, predictor_actions):
+    with tf.variable_scope(self.predictor_model_scope, reuse=tf.AUTO_REUSE):
+      predictor_model_rescaled_obses = predictor_model_rescaled_obses[:,:,:,-1:]
+      predictor_model_embeddings = self.predictor.encoder(predictor_model_rescaled_obses, embedding_activation=None, reuse=tf.AUTO_REUSE)
+      predictor_model_outputs = self.predictor.decoder(predictor_model_embeddings)
+      predictor_reconstruction_errors = self.predictor.reconstruction_errors(predictor_model_rescaled_obses, predictor_model_outputs)
+      predictor_reconstruction_loss = tf.reduce_mean(predictor_reconstruction_errors)
+      predictor_embedding_loss = self.predictor.embedding_loss(predictor_model_embeddings)
+    with tf.variable_scope(self.predictor_model_scope + "_internal", reuse=tf.AUTO_REUSE):
+      predictor_extras = tf.zeros(0)
+      predictor_rewards = 10 * tf.sqrt(predictor_reconstruction_errors / 84 / 84)
+      predictor_train_loss = predictor_reconstruction_loss + predictor_embedding_loss
+    return predictor_extras, predictor_rewards, predictor_train_loss
+
+  def build_inverse_forward_predictor_model(self, predictor_model_rescaled_obses, predictor_actions, beta=0.2):
+    inverse_predictor_extras, inverse_predictor_rewards, inverse_predictor_train_loss = self.build_inverse_predictor_model(predictor_model_rescaled_obses, predictor_actions)
+    forward_predictor_extras, forward_predictor_rewards, forward_predictor_train_loss = self.build_forward_predictor_model(predictor_model_rescaled_obses, predictor_actions)
+
+    with tf.variable_scope(self.predictor_model_scope + "_internal", reuse=tf.AUTO_REUSE):
+      predictor_extras = tf.stack([inverse_predictor_train_loss, forward_predictor_train_loss])
+      predictor_rewards = forward_predictor_rewards 
+      predictor_train_loss = (1-beta) * inverse_predictor_train_loss + beta * forward_predictor_train_loss
+ 
+    return predictor_extras, predictor_rewards, predictor_train_loss
+
+  def build_forward_predictor_model(self, predictor_model_rescaled_obses, predictor_actions):
+    with tf.variable_scope(self.predictor_model_scope, reuse=tf.AUTO_REUSE):
+      predictor_model_embeddings = [self.predictor.encoder(predictor_model_rescaled_obses[:,:,:,i:i+1], embedding_activation=None, reuse=tf.AUTO_REUSE) for i in range(self.num_images)]
+    with tf.variable_scope(self.predictor_model_scope + "_internal", reuse=tf.AUTO_REUSE):
+      predictor_net = tf.layers.dense(tf.concat(predictor_model_embeddings[:-1] + [tf.one_hot(predictor_actions, self.num_actions)], -1), 256, activation=tf.nn.relu, name="forward_fc1")
+      predictor_embedding = tf.layers.dense(predictor_net, self.predictor.embedding_size, name="forward_fc2")
+      predictor_forward_errors = 0.5*tf.reduce_sum(tf.square(tf.subtract(predictor_embedding, tf.stop_gradient(predictor_model_embeddings[-1]))), -1)
+      predictor_extras = tf.expand_dims(tf.sqrt(tf.reduce_mean(tf.square(predictor_model_embeddings[-1]), -1)),-1)
+      predictor_rewards = predictor_forward_errors
+      predictor_train_loss = tf.reduce_mean(predictor_forward_errors)
+    return predictor_extras, predictor_rewards, predictor_train_loss
+
+  def build_inverse_predictor_model(self, predictor_model_rescaled_obses, predictor_actions):
+    with tf.variable_scope(self.predictor_model_scope, reuse=tf.AUTO_REUSE):
+      predictor_model_embeddings = [self.predictor.encoder(predictor_model_rescaled_obses[:,:,:,i:i+1], embedding_activation=None, reuse=tf.AUTO_REUSE) for i in range(self.num_images)]
+    with tf.variable_scope(self.predictor_model_scope + "_internal", reuse=tf.AUTO_REUSE):
+      predictor_net = tf.layers.dense(tf.concat(predictor_model_embeddings, -1), 256, activation=tf.nn.relu, name="inverse_fc1")
+      predictor_logits = tf.layers.dense(predictor_net, self.num_actions, name="inverse_fc2")
+      predictor_inverse_errors = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=predictor_logits, labels=predictor_actions)
+      predictor_extras = tf.nn.softmax(predictor_logits,-1)
+      predictor_rewards = predictor_inverse_errors
+      predictor_train_loss = tf.reduce_mean(predictor_inverse_errors)
+    return predictor_extras, predictor_rewards, predictor_train_loss
+  
   def initialize(self):
     if self.embedder_dir:
       embedder_saver = tf.train.Saver(var_list=tf.trainable_variables(self.embedder_model_scope))
@@ -50,11 +113,20 @@ class StateEncoder:
     else:
       embedder_embeddings = [ None for env_idx in range(len(obses)) ]
     if self.predictor_dir:
-      predictor_start_timestamp = datetime.datetime.now()
-      predictor_prob_values, predictor_inverse_errors_value, _, predictor_global_step_value = self.sess.run([self.predictor_probs, self.predictor_inverse_errors, self.predictor_train_step, self.predictor_global_step], { self.predictor_model_obses1:np.expand_dims(obses[:,:,:,-1],-1), self.predictor_model_obses2:np.expand_dims(obses[:,:,:,-2],-1), self.predictor_actions:actions })
-      predictor_rewards = predictor_inverse_errors_value
-      predictor_stop_timestamp = datetime.datetime.now()
-      print("STATE_ENCODER_PREDICTOR: timestamp=%s step=%s inverse_errors=%s actions=%s action_probs=%s duration=%ssec" % (predictor_stop_timestamp, predictor_global_step_value, predictor_inverse_errors_value, actions, list([list(ap) for ap in predictor_prob_values]), (predictor_stop_timestamp - predictor_start_timestamp).total_seconds()))
+      predictor_eval_start_timestamp = datetime.datetime.now()
+      eval_predictor_reward_values, eval_predictor_extra_values, predictor_global_step_value  = self.sess.run([ self.eval_predictor_rewards, self.eval_predictor_extras, self.predictor_global_step ], { self.eval_predictor_model_obses:obses, self.eval_predictor_actions:actions })
+      predictor_eval_stop_timestamp = datetime.datetime.now()
+      print("STATE_ENCODER_PREDICTOR_EVAL: timestamp=%s step=%s rewards=%s actions=%s extras=%s duration=%ssec" % (predictor_eval_stop_timestamp, predictor_global_step_value, eval_predictor_reward_values, actions, eval_predictor_extra_values.tolist(), (predictor_eval_stop_timestamp - predictor_eval_start_timestamp).total_seconds()))
+      predictor_rewards = eval_predictor_reward_values
+
+      predictor_train_start_timestamp = datetime.datetime.now()
+      self.predictor_replay_buffer.extend(zip(obses.tolist(), actions))
+      if len(self.predictor_replay_buffer) > self.predictor_replay_size:
+        self.predictor_replay_buffer = self.predictor_replay_buffer[-self.predictor_replay_size:]
+      train_obses, train_actions = zip(*[random.choice(self.predictor_replay_buffer) for i in range(self.predictor_batch_size * len(actions))])
+      predictor_train_loss_value, _ = self.sess.run([self.predictor_train_loss, self.predictor_train_step], { self.train_predictor_model_obses:train_obses, self.train_predictor_actions:train_actions })
+      predictor_train_stop_timestamp = datetime.datetime.now()
+      print("STATE_ENCODER_PREDICTOR_TRAIN: timestamp=%s step=%s train_loss=%s duration=%ssec" % (predictor_train_stop_timestamp, predictor_global_step_value, predictor_train_loss_value, (predictor_train_stop_timestamp - predictor_train_start_timestamp).total_seconds()))
     else:
       predictor_rewards = [ 0.0 for env_idx in range(len(obses)) ]
     return embedder_embeddings, predictor_rewards 
