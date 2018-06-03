065e6b424d6d6f30333f4527da326361dca174af
Oleg Mürk <oleg.myrk@gmail.com>
2018-06-03 17:51:42 +0000

    curriculum development

--- agents/exploration/curriculum_env.py
+++ agents/exploration/curriculum_env.py
@@ -54,28 +54,32 @@ class CurriculumEnv(gym.Wrapper):
      while not success:
        attempt += 1
 
-       curriculum_progress = min(1.0,self.total_steps / max(1.0,float(self.max_curriculum_steps)))
-       #replay_length = math.floor(random.random() * (1-curriculum_progress) * self.movie_length)
-       replay_length = max(0,min(self.movie_length, math.floor((1-curriculum_progress) * self.movie_length)))
-       replay_length = math.floor(replay_length * 0.99 ** attempt)
+       try:
+         curriculum_progress = min(1.0,self.total_steps / max(1.0,float(self.max_curriculum_steps)))
+         #replay_length = math.floor(random.random() * (1-curriculum_progress) * self.movie_length)
+         replay_length = max(0,min(self.movie_length, math.floor((1-curriculum_progress) * self.movie_length)))
+         replay_length = math.floor(replay_length * 0.99 ** attempt)
 
-       print("CURRICULUM_REPLAY_BEGIN: total_steps=%s total_replay_steps=%s episode=%s attempt=%s curriculum_progress=%s movie_length=%s replay_length=%s" % (self.total_steps, self.total_replay_steps, self.episode, attempt, curriculum_progress, self.movie_length, replay_length), file=self.log_file)
+         print("CURRICULUM_REPLAY_BEGIN: total_steps=%s total_replay_steps=%s episode=%s attempt=%s curriculum_progress=%s movie_length=%s replay_length=%s" % (self.total_steps, self.total_replay_steps, self.episode, attempt, curriculum_progress, self.movie_length, replay_length), file=self.log_file)
 
-       movie, obs = self._replay_reset()
+         movie, obs = self._replay_reset()
 
-       done = False
-       replay_steps = 0
-       replay_reward = 0
-       while not done and replay_steps < replay_length and movie.step():
-         self.total_replay_steps += 1
-         replay_steps += 1
-         obs, reward, done, info, action_keys = self._replay_step(movie)
-         print("CURRICULUM_STEP: total_steps=%s total_replay_steps=%s episode=%s attempt=%s movie_length=%s replay_length=%s replay_steps=%s action_keys=%s reward=%s replay_reward=%s done=%s info=%s" % (self.total_steps, self.total_replay_steps, self.episode, attempt, self.movie_length, replay_length, replay_steps, action_keys, reward, replay_reward, done, info), file=self.log_file)
-         replay_reward += reward
+         done = False
+         replay_steps = 0
+         replay_reward = 0
+         while not done and replay_steps < replay_length and movie.step():
+           self.total_replay_steps += 1
+           replay_steps += 1
+           obs, reward, done, info, action_keys = self._replay_step(movie)
+           print("CURRICULUM_STEP: total_steps=%s total_replay_steps=%s episode=%s attempt=%s movie_length=%s replay_length=%s replay_steps=%s action_keys=%s reward=%s replay_reward=%s done=%s info=%s" % (self.total_steps, self.total_replay_steps, self.episode, attempt, self.movie_length, replay_length, replay_steps, action_keys, reward, replay_reward, done, info), file=self.log_file)
+           replay_reward += reward
        
-       if not done:
-         success = True
-         self.replay_reward = replay_reward
+         if not done:
+           success = True
+           self.replay_reward = replay_reward
+       except RuntimeError as e:
+         import traceback
+         traceback.print_exc()
   
      print("CURRICULUM_REPLAY_END: total_steps=%s total_replay_steps=%s episode=%s attempt=%s movie_length=%s replay_length=%s replay_reward=%s" % (self.total_steps, self.total_replay_steps, self.episode, attempt, self.movie_length, replay_length, replay_reward), file=self.log_file)
      self.log_file.flush()
@@ -83,7 +87,12 @@ class CurriculumEnv(gym.Wrapper):
 
    def step(self, action):
      self.total_steps += 1
-     obs, reward, done, info = super().step(action)
+     try:
+       obs, reward, done, info = super().step(action)
+     except RuntimeError as e:
+       import traceback
+       traceback.print_exc()
+       raise
      if self.replay_reward:
        info['initial_reward'] = self.replay_reward
        self.replay_reward = None
