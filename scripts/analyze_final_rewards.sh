be1ebec491281f6eae44030cf392ae2f79da0ee0
Oleg Mürk <oleg.myrk@gmail.com>
2018-06-12 21:59:39 +0000

    some cleanups

--- scripts/analyze_final_rewards.sh
+++ scripts/analyze_final_rewards.sh
@@ -1,1 +1,1 @@
-bash analyze.sh $1 | grep total_reward | sed -e "s/.*total_reward=//" | sed -e "s/ .*//"
+bash scripts/analyze.sh $1 | grep total_reward | sed -e "s/.*total_reward=//" | sed -e "s/ .*//"
--- scripts/analyze_rewards.sh
+++ scripts/analyze_rewards.sh
@@ -1,1 +1,1 @@
-bash analyze.sh $1 | grep avg_episode_reward= | sed -e s"/^.*avg_episode_reward=//"
+bash scripts/analyze.sh $1 | grep avg_episode_reward= | sed -e s"/^.*avg_episode_reward=//" | sed -e "s/ .*//"
